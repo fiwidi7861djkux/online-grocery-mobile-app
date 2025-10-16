8c6a10372d3d0371e7ae2e9fd15a39ef1b78a9ba
fiwidi7861djkux <fiwidi7861djkux.com@proton.me>
2025-10-16 11:13:43 -0700

    Add comprehensive backend tests with Jest

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+// ESLint configuration
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  }
+};
